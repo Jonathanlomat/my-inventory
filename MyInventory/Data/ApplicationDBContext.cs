fbb019f60bcadb475abe3e0f2fa50c93e6a1bf39
Jonathan Lomat <11817896@benilde.edu.ph>
2020-11-03 10:01:54 +0800

    Initial Commit

---
+++ MyInventory/Data/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using MyInventory.Models;
+
+namespace MyInventory.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
+
